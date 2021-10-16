3066acb4736ec2539585c7c09aea4cd66f4665f1
Leonel <leeonel_@live.com>
2021-10-16 19:59:23 -0300

    primercommit

---
+++ src/menu.c
@@ -0,0 +1,16 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+
+void menu(void){
+	printf(	"1-Dar alta empleado\n"
+			"2-Modificar empleado\n"
+			"3-Dar baja empleado\n"
+			"4-Informar\n"
+			"5-Salir\n");
+
+}
